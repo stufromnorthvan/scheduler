19e04414cdfb45647491f0407e3f32d7483b6ccd
stufromnorthvan <stumitchell@irsss.ca>
2022-11-02 21:07:48 -0700

    add create and edit form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+import { action } from '@storybook/addon-actions';
+
+export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = function() {
+    setStudent("")
+    setInterviewer("")
+  }
+  const cancel = () => {
+    reset()
+    props.onCancel(`Cancelled`)
+  }
+  const save = () => {
+    props.onSave(`${student}, ${interviewer}`)
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value = {interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
